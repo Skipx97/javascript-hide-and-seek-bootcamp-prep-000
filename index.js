12b40b84ef2ae04ee0bac4b6481d61fc631ee48a
Derrick Skipwith <dskipwith15@gmail.com>
2018-07-01 11:46:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
+<<<<<<< HEAD
 
   for (let i = 0; i < rankedLists.length; i++) {
     let x = rankedLists[i].children
@@ -18,12 +19,23 @@ function increaseRankBy(n) {
       x[y].innerHTML = parseInt(x[y].innerHTML) + n
     }
 
+=======
+  
+  for (let i = 0; i < rankedLists.length; i++) {
+    let x = rankedLists[i].children
+    
+    for (let y = 0; y < x.length; y++) {
+      x[y].innerHTML = parseInt(x[y].innerHTML) + n
+    }
+    
+>>>>>>> 300c9a4445541dcaf93ac3927f7b8ac104edfd43
   }
 }
 
 function deepestChild() {
   var x = document.getElementById('grand-node');
   let y = x.children[0];
+<<<<<<< HEAD
 
  while (y) {
     x = y
@@ -32,3 +44,13 @@ function deepestChild() {
  }
 return x
 }
+=======
+  
+ while (y) {
+    x = y
+   y = x.children[0]
+   
+ }
+return x
+}
+>>>>>>> 300c9a4445541dcaf93ac3927f7b8ac104edfd43
